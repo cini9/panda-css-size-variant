7fa4e153dc26115d1ba51d2ff86d1e86376936de
Sinan Ucak <ucak.sinan@gmail.com>
2023-11-30 14:40:34 +0100

    corrected PR comments

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en' className={`${ppNeueMontreal.variable}`}>
+    <html lang='en' className={ppNeueMontreal.variable}>
       <body>{children}</body>
     </html>
   )
